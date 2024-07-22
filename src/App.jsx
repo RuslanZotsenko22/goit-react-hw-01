285d59179a86ce5362f06817c07605125dce9b94
Ruslan Zotsenko <justmymail989@gmail.com>
2024-07-22 20:25:20 +0200

    My work done

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import Profile from './components/Profile/Profile/Profile';
 import FriendList from './components/Profile/Friends/FriendList';
 import userData from './components/Profile/Profile/Profile.json';
 import friends from './components/Profile/Friends/FrendList.json';
+import TransactionHistory from './components/Profile/transactions/TransactionHistory';
+import transaction from './components/Profile/transactions/transaction.json';
 import './App.css';
 
-const App = () => {
-  return (
-    <div>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <Profile
+      name={userData.username}
+      tag={userData.tag}
+      location={userData.location}
+      image={userData.avatar}
+      stats={userData.stats}
+    />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transaction} />
+  </div>
+);
 
 export default App;
 
